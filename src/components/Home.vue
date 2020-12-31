331f31c8deb05c971f6fe40f8be27172f38c10fb
fws <1354801004@qq.com>
2020-12-31 19:06:19 +0800

    后端环境包括路由配置

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,23 +22,11 @@
                 <span>{{menu.title}}</span>
               </template>
 
-              <!--循环二级菜单-->
-              <el-menu-item :index="smenu.path" v-for="smenu in menu.sList" :key="smenu.id" @click="saveNavState(smenu.path)">
-                <template slot="title"><span>
-                  {{smenu.title}}
-                </span> </template>
-
-              </el-menu-item>
 
 
             </el-submenu>
-
-
           </el-menu>
-
-
         </el-aside>
-
         <el-main>
           <!--显示内容页面，占位符  router-view-->
           <router-view></router-view>
@@ -52,11 +40,99 @@
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  import axios from "axios"
+
+  export default {
+    name: "Home",
+    data() {
+      return {
+        menuList: [],
+        isCollapse:false,
+        activePath :"/welcome"
+      }
+    },
+
+    methods: {
+      getMenuList: function () {
+        axios.get("/getAllMenu").then(res => {
+          if (res.data.status == "200") {
+            this.menuList = res.data.data;
+
+          } else {
+            this.$message.error("数据获取失败")
+          }
+        })
+      },
+      toggleCollapase:function () {
+        this.isCollapse = !this.isCollapse
+      },
+      saveNavState:function (activePath) {
+        this.activePath = activePath;
+        sessionStorage.setItem('activePath',activePath);
+      }
+    },
+
+    created() {
+      this.getMenuList();
+      this.activePath = sessionStorage.getItem("activePath")
     }
+
+  }
 </script>
 
 <style scoped>
+  .home-container {
+    height: 100%;
+  }
+
+  .el-header {
+    background-color: #373d41;
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0%;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+  }
+
+  .el-aside {
+    background-color: #333744;
+  }
+
+  .el-aside el-menu {
+    border-right: none;
+  }
+
+  .el-main {
+    background-color: #eaedf1;
+  }
+
+  .el-header div {
+    display: flex;
+    align-items: center;
+  }
+
+  .el-header div span {
+    margin-left: 15px;
+  }
+
+  .logo_img {
+    width: 55px;
+    height: 55px;
+  }
+
+  .toggle-button {
+    background-color: #4A5064;
+    font-size: 10px;
+    line-height: 24px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+  }
+
+
+
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,70 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+
+//import Login from '@/components/login'
+import Home from '@/components/Home'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+  {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Home',
+      component: Home
+    },
+   /*   {
+      path:'/studentManagement',
+      name:'',
+      component:Login
+
+    },
+    {
+      path:'/teacherManagement',
+      name:'EmpMain',
+      component:EmpMain
+
+    },
+    {
+      path:'/deptevaluatorManagement',
+      name:'EmpMain',
+      component:EmpMain
+
+    },
+    {
+      path:'/courseMaintain',
+      name:'EmpMain',
+      component:EmpMain
+
+    },
+    {
+      path:'/deptNaintain',
+      name:'EmpMain',
+      component:EmpMain
+
+    },
+    {
+      path:'/resetPassword',
+      name:'EmpMain',
+      component:EmpMain
+
+    },
+    {
+      path:'/studentRating',
+      name:'EmpMain',
+      component:EmpMain
+
+    },
+    {
+      path:'/studentEvaluation',
+      name:'EmpMain',
+      component:EmpMain
+
+    },
+
+
+*/
   ]
 })
